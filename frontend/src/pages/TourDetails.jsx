5498dc4b5dc1c346a502eec0daa5e01b13ca0bc8
Shivam Mishra <coder.mishrashivam@gmail.com>
2024-10-08 23:16:52 +0530

    Integrated the Tour and tourDetails. Fetching data from database

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,22 +1,35 @@
-import React,{useRef, useState} from 'react';
+import React,{useEffect, useRef, useState} from 'react';
 import '../styles/tour-details.css';
 import {Container, Row, Col, Form, ListGroup} from 'reactstrap';
 import {useParams} from 'react-router-dom';
-import tourData from '../assets/data/tours';
 import calculateAvgRating from './../utils/avgRating';
 import avatar from '../assets/images/avatar.jpg';
 import Booking from '../components/Booking/Booking';
 import Newsletter from '../shared/Newsletter';
+import useFetch from '../hooks/useFetch';
+import { BASE_URL } from '../utils/config';
 
 const TourDetails = () => {
 
   const {id} = useParams();
   const reviewsMsgRef = useRef('');
   const [tourRating, setTourRating] = useState(null);
-  //static data
-  const tour = tourData.find(tour=> tour.id === id);
+  // Fetch data from database
+  const { 
+    data:tour, 
+    loading, 
+    error } = useFetch(`${BASE_URL}/tours/${id}`);
+
   //destructure properties from tour project
-  const {photo, title, desc, price, address, reviews, city, distance, maxGroupSize} = tour;
+  const {photo, 
+    title, 
+    desc, 
+    price, 
+    address, 
+    reviews, 
+    city, 
+    distance, 
+    maxGroupSize} = tour;
   const {totalRating, avgRating} = calculateAvgRating(reviews);
   //formate date
   const options = {day:'numeric', month:'long', year:'numeric'};
@@ -26,14 +39,23 @@ const TourDetails = () => {
     const reviewText = reviewsMsgRef.current.value
 
     // alert(`${reviewText}, ${tourRating}`);
-    //later will call api
+    //later will call our api
   }
+
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  },[tour]);
 
   return (
     <>
       <section>
         <Container>
-          <Row>
+
+          { loading && <h4 className='text-center pt-5'>Loading......</h4>}
+          { error && <h4 className='text-center pt-5'>{error}</h4> }
+
+          {
+            !loading && !error &&           <Row>
             <Col lg='8'>
               <div className="tour__content">
                 <img src={photo} alt="" />
@@ -121,6 +143,8 @@ const TourDetails = () => {
             </Col>
             
           </Row>
+            
+          }
         </Container>
       </section>
       <Newsletter />
