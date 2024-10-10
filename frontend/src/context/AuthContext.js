0b93855d692314f55fbebbcd54d02290c13585c5
Shivam Mishra <coder.mishrashivam@gmail.com>
2024-10-10 22:25:27 +0530

    impolemented the alternative logic for getting user of locastorage

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,24 @@
 import {createContext, useEffect, useReducer} from 'react';
 
+// const getUserFromLocalStorage = () => {
+//     const user = localStorage.getItem('user');
+//     if (user) {
+//         try {
+//             return JSON.parse(user);
+//         } catch (error) {
+//             console.error('Error parsing user from localStorage:', error);
+//             return null; // Return null if parsing fails
+//         }
+//     }
+//     return null; // Return null if user is not found
+// };
+
+// const initial_state = {
+//     user: getUserFromLocalStorage(),
+//     loading: false,
+//     error: null,
+// };
+
 const initial_state = {
     user: 
     localStorage.getItem('user') !== undefined 
